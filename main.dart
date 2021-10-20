12b23763fc5ceaf40e080253ac45024425ad016e
Thoxh <38158101+Thoxh@users.noreply.github.com>
2021-10-20 18:48:00 +0200

    default_main_for_flutter_project

---
+++ main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => HomePage(),
+        // "/settings": (context) => Settings(),
+      },
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(defaultPadding),
+        ),
+      ),
+    );
+  }
+}
